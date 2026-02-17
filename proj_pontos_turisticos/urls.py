6a558d1674daa85b8c4353d0588b53b05c1b7dbd
Rogério B. Martins <rogeriodelphi@gmail.com>
2026-02-17 03:16:10 -0300

    Criação da api para Avaliações

--- proj_pontos_turisticos/urls.py
+++ proj_pontos_turisticos/urls.py
@@ -4,12 +4,14 @@ from rest_framework import routers
 from core.api.viewsets import PontoTuristicoViewSet
 from enderecos.api.viewsets import EnderecoViewSet
 from atracoes.api.viewsets import AtracaoViewSet
+from avaliacoes.api.viewsets import AvaliacaoViewSet
 
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
 router.register(r'pontoturisticos', PontoTuristicoViewSet, basename='PontoTuristico')
 router.register(r'enderecos', EnderecoViewSet, basename='Endereco')
 router.register(r'atracoes', AtracaoViewSet, basename='Atracao')
+router.register(r'avaliacoes', AvaliacaoViewSet, basename='Avaliacao')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
