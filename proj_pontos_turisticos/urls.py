fd83de2c1a0dee4c6d5295f1336b74cea8afef24
Rogério B. Martins <rogeriodelphi@gmail.com>
2026-02-17 03:11:37 -0300

    Criação da api para Atrações

--- proj_pontos_turisticos/urls.py
+++ proj_pontos_turisticos/urls.py
@@ -3,11 +3,13 @@ from django.urls import path, include
 from rest_framework import routers
 from core.api.viewsets import PontoTuristicoViewSet
 from enderecos.api.viewsets import EnderecoViewSet
+from atracoes.api.viewsets import AtracaoViewSet
 
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
 router.register(r'pontoturisticos', PontoTuristicoViewSet, basename='PontoTuristico')
 router.register(r'enderecos', EnderecoViewSet, basename='Endereco')
+router.register(r'atracoes', AtracaoViewSet, basename='Atracao')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
