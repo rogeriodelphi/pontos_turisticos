10c660a4331f55a346c3ebb464dcb897ae161f7c
Rogério B. Martins <rogeriodelphi@gmail.com>
2026-02-17 02:59:10 -0300

    Criação das apis para Ponto Turísticos e Enderecos

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
+from rest_framework.viewsets import ModelViewSet
+
 from .models import PontoTuristico
 
-
-admin.site.register(PontoTuristico)
+@admin.register(PontoTuristico)
+class PontoTuristico(admin.ModelAdmin):
+    list_display = ['id', 'nome', 'descricao', 'aprovado', 'endereco']
--- enderecos/admin.py
+++ enderecos/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Endereco
 
-
-admin.site.register(Endereco)
+@admin.register(Endereco)
+class EnderecoAdmin(admin.ModelAdmin):
+    list_display = ['id', 'linha1', 'linha2', 'cidade', 'estado', 'pais', 'latitude', 'longitude']
